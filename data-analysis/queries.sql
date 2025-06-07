9a94e6048c4e7b46aed178f478145a54b542d6ff
Adith Venkatesh <avenka29@ncsu.edu>
2025-06-06 22:34:13 -0400

    Updated queries

--- data-analysis/queries.sql
+++ data-analysis/queries.sql
@@ -38,16 +38,29 @@ BBantamweightRank REAL,BFlyweightRank REAL,BPFPRank REAL,BetterRank TEXT,Finish 
 FinishRound REAL,FinishRoundTime TEXT,TotalFightTimeSecs REAL, RedDecOdds REAL,BlueDecOdds REAL,
 RSubOdds REAL,BSubOdds REAL,RKOOdds REAL,BKOOdds REAL);
 
+
+--Creating fighter database
+Create TABLE Fighters(Name TEXT, id SERIAL Primary Key)
+
+
+--Populate fighters table
+SELECT fighter
+FROM (
+    SELECT RedFighter  AS fighter FROM Matches
+    UNION
+    SELECT BlueFighter AS fighter FROM Matches
+) AS fighters
+ORDER BY fighter;
+Create table fighters(Name TEXT, id SERIAL Primary Key)
+
 -- Create fighters_data table
 CREATE TABLE Fighters_Data(Name INTEGER REFERENCES Fighters(id), recent_match INTEGER REFERENCES Matches(id), weight_class TEXT,
 Gender GENDER, current_win_streak SMALLINT, current_lose_streak SMALLINT, Draws SMALLINT,
 total_rounds_fought REAL, total_title_bouts SMALLINT, wins_by_decision_majority SMALLINT,
 wins_by_decision_split SMALLINT, wins_by_decision_unanimous SMALLINT, wins_by_ko SMALLINT,
-wins_by_sub SMALLINT, Wins SMALLINT, Stance TEXT, Height REAL, Reach REAL, Age SMALLINT);
+wins_by_sub SMALLINT, Loses SMALLINT, Wins SMALLINT, Stance TEXT, Height REAL, Reach REAL, Age SMALLINT);
 
 
---Creating fighter database
-Create TABLE Fighters(Name TEXT, id SERIAL Primary Key)
 
 --Populating fighters_data database
 DO
@@ -75,12 +88,13 @@ BEGIN
         Gender, current_win_streak, current_lose_streak, Draws,
         total_rounds_fought, total_title_bouts, wins_by_decision_majority,
         wins_by_decision_split, wins_by_decision_unanimous, wins_by_ko,
-        wins_by_sub, Wins, Stance, Height, Reach, Age)
+        wins_by_sub, Loses, Wins, Stance, Height, Reach, Age)
 
         VALUES(fighter.id, recent_match.id, recent_match.WeightClass, recent_match.Gender, recent_match.RedCurrentWinStreak,
         recent_match.RedCurrentLoseStreak, recent_match.RedDraws, recent_match.RedTotalRoundsFought,
         recent_match.RedTotalTitleBouts, recent_match.RedWinsByDecisionMajority, recent_match.RedWinsByDecisionSplit,
-        recent_match.RedWinsByDecisionUnanimous, recent_match.RedWinsByKO, recent_match.RedWinsBySubmission,
+        recent_match.RedWinsByDecisionUnanimous, recent_match.RedWinsByKO, recent_match.RedWinsBySubmission, 
+        recent_match.RedLosses,
         recent_match.RedWins, recent_match.RedStance, recent_match.RedHeightCms,
         recent_match.RedReachCms, recent_match.RedAge);
     ELSE
@@ -88,12 +102,13 @@ BEGIN
         Gender, current_win_streak, current_lose_streak, Draws,
         total_rounds_fought, total_title_bouts, wins_by_decision_majority,
         wins_by_decision_split, wins_by_decision_unanimous, wins_by_ko,
-        wins_by_sub, Wins, Stance, Height, Reach, Age)
+        wins_by_sub, Loses, Wins, Stance, Height, Reach, Age)
 
         VALUES(fighter.id, recent_match.id, recent_match.WeightClass, recent_match.Gender, recent_match.BlueCurrentWinStreak,
             recent_match.BlueCurrentLoseStreak, recent_match.BlueDraws, recent_match.BlueTotalRoundsFought,
             recent_match.BlueTotalTitleBouts, recent_match.BlueWinsByDecisionMajority, recent_match.BlueWinsByDecisionSplit,
             recent_match.BlueWinsByDecisionUnanimous, recent_match.BlueWinsByKO, recent_match.BlueWinsBySubmission,
+            recent_match.BlueLosses,
             recent_match.BlueWins, recent_match.BlueStance, recent_match.BlueHeightCms,
             recent_match.BlueReachCms, recent_match.BlueAge);
     END IF;
@@ -101,3 +116,54 @@ BEGIN
 END;
 $body$
 LANGUAGE plpgsql;
+
+INSERT INTO fighters
+SELECT fighter
+FROM (
+    SELECT RedFighter  AS fighter FROM Matches
+    UNION
+    SELECT BlueFighter AS fighter FROM Matches
+) AS fighters
+
+
+-- [TODO] START HERE FOR NEW STUFF
+-- Creating fighters-new table
+CREATE TABLE fighters(fighter_id TEXT, name TEXT, birthdate DATE, nickname TEXT, association TEXT,
+weight_class TEXT, wins REAL, losses REAL, draws REAL,
+wins_by_ko_tko REAL, wins_by_submission REAL, wins_by_decision REAL, wins_by_other REAL,
+losses_by_ko_tko REAL, losses_by_submission REAL, losses_by_decision REAL, losses_by_other REAL,
+country TEXT, city TEXT, weight REAL, sex VARCHAR(1), height_ft REAL, height_cm REAL);
+
+-- Add serial id column to fighters
+ALTER TABLE fighters
+ADD COLUMN id SERIAL PRIMARY KEY;
+
+
+--Adding reach to new table
+ALTER TABLE fighters_new
+ADD COLUMN reach_cms REAL;
+
+UPDATE fighters_new
+SET reach_cms = fighters_data.reach
+FROM
+fighters_data JOIN fighters
+ON fighters.id = fighters_data.fighter_id
+WHERE fighters.name = fighters_new.name;
+
+
+--Adding age to new table
+ALTER TABLE fighters_new
+ADD COLUMN age SMALLINT;
+
+UPDATE fighters_new
+SET age = fighters_data.age
+FROM
+fighters_data JOIN fighters
+ON fighters.id = fighters_data.fighter_id
+WHERE fighters.name = fighters_new.name;
+
+
+--Drop original height column
+ALTER TABLE fighters_new
+DROP COLUMN height_cm
+
